9aa366c6872f2b699cdfa331c2061b791cb165d8
Mohamed Taibi <mohamed.taibi8506@gmail.com>
2023-10-09 23:18:14 +0200

    refactor analytics counter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,42 +1,87 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
+import reader.ISymptomReader;
+import writer.ISymptomWriter;
+
+/**
+ * 
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	
-	private static int rashCount = 0;		
-	private static int pupilCount = 0;		
-	
-	public static void main(String args[]) throws Exception {
-		try (// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"))) {
-			String line = reader.readLine();
 
-			while (line != null) {
-				
-				System.out.println("symptom from file: " + line);
-				
-				if (line.equals("headache")) {
-					headacheCount++;
-					System.out.println("number of headaches: " + headacheCount);
-				}
-				else if (line.equals("rash")) {
-					rashCount++;
-				}
-				else if (line.contains("pupils")) {
-					pupilCount++;
-				}
+	//Variables
+	private ISymptomReader symptomReader;
+	private ISymptomWriter symptomWriter;	
 
-				line = reader.readLine();	// get another symptom
+	//Constructeur
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+		super();
+		this.symptomReader = reader;
+		this.symptomWriter = writer;
+	}
+
+	//Méthodes
+	/**
+	 * @return List<String>
+	 */
+	public List<String> getSymptoms() { 
+		return symptomReader.getSymptoms();
+	}
+
+
+	/**
+	 * @param List<String> symptoms
+	 * @return Map<String, Integer> symptomCounts
+	 */
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+
+		// Créez une nouvelle HashMap pour stocker les symptomes et occurrences
+		Map<String, Integer> symptomCounts = new HashMap<>();
+
+		// Parcourez la liste des symptômes
+		for (String symptom : symptoms) {
+			// Vérifiez si le symptôme existe déjà dans la map
+			if (symptomCounts.containsKey(symptom)) {
+				// S'il existe, récupérez le compteur actuel
+				int currentCount = symptomCounts.get(symptom);
+				// Incrémente le compteur
+				symptomCounts.put(symptom, currentCount + 1);
+			} else {
+				// S'il n'existe pas, ajoutez-le avec un comptage initial de 1
+				symptomCounts.put(symptom,1);
 			}
 		}
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+
+
+		return symptomCounts;
 	}
+
+
+
+	/**
+	 * @param Map<String, Integer> symptomCounts
+	 * @return TreeMap<String, Integer> sortedSymptoms
+	 */
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptomCounts) {
+		// Instancier une TreeMap en lui passant la Map non triée en argument
+		TreeMap<String, Integer> sortedSymptoms = new TreeMap<>(symptomCounts);
+
+		// Le TreeMap triera automatiquement les clés (symptômes) par ordre alphabétique
+
+		return sortedSymptoms;
+	}	
+
+	/**
+	 * @param Map<String, Integer> symptoms
+	 */
+	public void writeSymptoms(Map<String, Integer> symptoms) { 
+		symptomWriter.writeSymptoms(symptoms);
+	}
+
+
+
 }
--- Project02Eclipse/src/writer/ISymptomWriter.java
+++ Project02Eclipse/src/writer/ISymptomWriter.java
@@ -6,8 +6,8 @@ import java.util.Map;
  * 
  */
 public interface ISymptomWriter {
-	
-	 /**
+
+	/**
 	 * @param symptoms
 	 */
 	public void writeSymptoms(Map<String, Integer> symptoms);
